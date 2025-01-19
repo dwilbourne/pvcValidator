4b4c18eb7f275701967eef61721ce1bd5445ef71
Doug Wilbourne <dougwilbourne@gmail.com>
2025-01-19 13:46:05 -0500

    added testers for IsNull and IsNotNull

---
+++ src/val_tester/null/IsNull.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author: Doug Wilbourne (dougwilbourne@gmail.com)
+ */
+
+declare(strict_types=1);
+
+namespace pvc\validator\val_tester\null;
+
+use pvc\interfaces\validator\ValTesterInterface;
+
+/**
+ * Class CanBeNull
+ */
+class IsNull implements ValTesterInterface
+{
+
+    public function testValue(mixed $value): bool
+    {
+        return is_null($value);
+    }
+}
